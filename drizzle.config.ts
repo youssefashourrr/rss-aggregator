1de620f63e324b3de5196f4efc2f373dcb0a1596
Youssef Ashour <youssef.ashraf.ashour@gmail.com>
2025-11-24 09:52:57 +0200

    feat: introduce drizzle migrations

---
+++ drizzle.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "drizzle-kit";
+import { readConfig } from "./src/config";
+
+
+export default defineConfig({
+  schema: "src/db/schema.ts",
+  out: "src/db/migrations",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: readConfig().dbUrl,
+  },
+});
